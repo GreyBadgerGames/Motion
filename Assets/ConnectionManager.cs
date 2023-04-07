49c4b15a9f1e829db8ad68ebdae7340570b5eb3a
ryanlaycock <ryan.laycock@hotmail.co.uk>
2023-04-07 20:04:24 +0100

    Renaming player based on main screen input

--- Assets/ConnectionManager.cs
+++ Assets/ConnectionManager.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ConnectionManager : NetworkBehaviour
 {
+    [SerializeField] private TMP_Text _nameInputField;
+
+    // ConnectionManager is spawned when the player joins (or creates) a server
     public override void OnNetworkSpawn()
     {
         if (IsServer && !string.IsNullOrEmpty("MainMenu"))
         {
+            NetworkManager.Singleton.LocalClient.PlayerObject.GetComponent<PersistentPlayerManager>().SetName(_nameInputField.text);
+
+            Debug.Log("Changing scene to Lobby");
             var status = NetworkManager.SceneManager.LoadScene("Lobby", LoadSceneMode.Single);
             
             if (status != SceneEventProgressStatus.Started)
