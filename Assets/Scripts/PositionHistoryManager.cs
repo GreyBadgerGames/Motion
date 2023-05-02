080bd6a150df758f0da7de06ee93868d8d31afa7
joelentondevo <jlenton@me.com>
2023-05-02 22:21:32 +0100

    add Positionhistorymanager for tracking previous player locations

--- Assets/Scripts/PositionHistoryManager.cs
+++ Assets/Scripts/PositionHistoryManager.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class PositionHistoryManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _playerObject;
+    public Vector3[] positionHistory = new Vector3[44];
+
     // Start is called before the first frame update
     void Start()
     {
-        Vector3[] lastposition = new Vector3[30];
 
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        UpdatePositionHistory(_playerObject.transform.position);
+        Debug.Log(positionHistory[0]);
+    }
+
+    void UpdatePositionHistory(Vector3 currentPosition)
+    {
+        //Loop through array and shift values one position left
+        for (int i = 0; i < positionHistory.Length; i++)
+        {
+            if (i != 0)
+            {
+                positionHistory[i-1] = positionHistory[i];
+            }
+            if (i == (positionHistory.Length - 1))
+            {
+                positionHistory[i] = currentPosition;
+            }
+        }
     }
 }
